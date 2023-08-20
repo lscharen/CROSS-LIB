ca88702984efedcdefc608e68f9c7f79f5f3a22d
Lucas Scharenbroich <lscharen@prowestgis.com>
2023-08-20 12:59:13 -0500

    Fix cc= command line usage in GoldenGate. Use pre-load header due to command line length restrictions

---
+++ src/cross_lib/cfg/orcac/preload.h
@@ -0,0 +1,15 @@
+#include "13:ORCACDefs:Defaults.h"
+
+#pragma path "cross_lib"
+#pragma path "cross_lib:include"
+#pragma path "cross_lib:input"
+#pragma path "cross_lib:display"
+#pragma path "cross_lib:display:tiles"
+#pragma path "cross_lib:rand"
+#pragma path "cross_lib:sound"
+#pragma path "cross_lib:sleep"
+#pragma path "cross_lib:text"
+#pragma path "cross_lib:graphics_mode"
+
+#define _XL_NO_JOYSTICK 1
+#define __APPLE2GS__ 1
